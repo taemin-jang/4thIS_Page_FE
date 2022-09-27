b83493455f9923f0e4627f012e4dac7420f4e841
0uizi0 <0uizi0@naver.com>
2022-09-27 20:18:20 +0900

    [기능 구현] question 페이지 contact us 부분 onclick시 url 연결 구현

--- src/js/question.js
+++ src/js/question.js
@@ -1,3 +1,7 @@
-$("div.title").on("click", function () {
+$("div.question").on("click", function () {
   $(this).next(".con").slideToggle(100);
+});
+
+$("div.kakao").on("click", function () {
+  location.href = "http://pf.kakao.com/_zbgxlxj";
 });
