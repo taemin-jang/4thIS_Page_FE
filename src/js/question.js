53fe898101c8b43ce5cdb52285431d355b525dee
0uizi0 <0uizi0@naver.com>
2022-10-15 01:58:26 +0900

    [소스 수정] question 상세 페이지 콘텐츠 간격 수정

--- src/js/question.js
+++ src/js/question.js
@@ -5,3 +5,23 @@ $("div.question").on("click", function () {
 $("div.kakao").on("click", function () {
   location.href = "http://pf.kakao.com/_zbgxlxj";
 });
+
+// $(document).ready(function () {
+//   $("#down").show();
+//   $("#up").hide();
+
+//   $("img.question__hover-icon").on("click", function () {
+//     $("#down").hide();
+//     $("#up").show();
+//     $("div.question").next(".con").slideToggle(100);
+//   });
+
+//   $("#up").click(function () {
+//     $("#down").show();
+//     $("#up").hide();
+//   });
+
+//   $("div.kakao").on("click", function () {
+//     location.href = "http://pf.kakao.com/_zbgxlxj";
+//   });
+// });
